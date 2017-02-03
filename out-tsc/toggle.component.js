558d85add4d85e38d982c3c98e922257eef99dc8
Craig Martin <craigmichaelmartin@gmail.com>
2017-02-03 13:43:11 -0500

    make dumb

--- out-tsc/toggle.component.js
+++ out-tsc/toggle.component.js
@@ -9,8 +9,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var Observable_1 = require('rxjs/Observable');
-var Subject_1 = require('rxjs/Subject');
 require('rxjs/add/operator/startWith');
 require('rxjs/add/operator/distinctUntilChanged');
 require('rxjs/add/operator/filter');
@@ -19,48 +17,21 @@ require('rxjs/add/operator/scan');
 var ToggleComponent = (function () {
     function ToggleComponent() {
         this.items = [];
-        this.unrelated = false;
+        this.disabledItemValues = [];
+        this.activeItemValues = [];
+        this.warningItemValues = [];
         this.toggleUpdated = new core_1.EventEmitter();
-        this.subscriptions = [];
     }
-    ToggleComponent.prototype.ngOnChanges = function (simpleChanges) {
-        // ngOnChanges is triggered according to strict equality
-        if (JSON.stringify(simpleChanges.items.currentValue) ===
-            JSON.stringify(simpleChanges.items.previousValue)) {
-            return;
-        }
-        this.cleanItems = this.items.map(function (item, i) {
+    ToggleComponent.prototype.ngOnInit = function () {
+        this.mappedItems = this.items.map(function (item, i) {
             return Object.assign({}, item, {
                 value: item.value != null ? item.value : item.text
             });
         });
     };
-    ToggleComponent.prototype.ngOnInit = function () {
-        // const defaultItem = this.cleanItems.find(item => item.default);
-        // this.startWith = defaultItem && defaultItem.value;
-        // this.falseState = this.cleanItems.reduce(
-        //   (obj, item) => Object.assign({}, obj, {[item.value]: false}),
-        //   {}
-        // );
-        // const startWithObj = this.cleanItems.reduce(
-        //   (obj, item) => Object.assign({}, obj, {[item.value]: !!item.default}),
-        //   {}
-        // );
-        // const startValue = Object.keys(startWithObj).filter(key => startWithObj[key]);
-        // this.storeSource.next({
-        //   type: 'SET_TOGGLE',
-        //   key: this.key,
-        //   value: this.unrelated ? startValue : startValue[0]
-        // });
-    };
     ToggleComponent.prototype.onClick = function (item) {
         if (!item.disabled) {
-            this.storeSource.next({
-                type: 'UPDATE_TOGGLE',
-                related: !this.unrelated,
-                key: this.key,
-                value: item.value
-            });
+            this.toggleUpdated.next(item);
         }
     };
     __decorate([
@@ -69,20 +40,16 @@ var ToggleComponent = (function () {
     ], ToggleComponent.prototype, "items", void 0);
     __decorate([
         core_1.Input(), 
-        __metadata('design:type', Boolean)
-    ], ToggleComponent.prototype, "unrelated", void 0);
+        __metadata('design:type', Object)
+    ], ToggleComponent.prototype, "disabledItemValues", void 0);
     __decorate([
         core_1.Input(), 
-        __metadata('design:type', Subject_1.Subject)
-    ], ToggleComponent.prototype, "storeSource", void 0);
+        __metadata('design:type', Object)
+    ], ToggleComponent.prototype, "activeItemValues", void 0);
     __decorate([
         core_1.Input(), 
-        __metadata('design:type', Observable_1.Observable)
-    ], ToggleComponent.prototype, "storeStream", void 0);
-    __decorate([
-        core_1.Input(), 
-        __metadata('design:type', String)
-    ], ToggleComponent.prototype, "key", void 0);
+        __metadata('design:type', Array)
+    ], ToggleComponent.prototype, "warningItemValues", void 0);
     __decorate([
         core_1.Output(), 
         __metadata('design:type', Object)
